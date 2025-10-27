d2d1d2d9b2f9add239435b59cbcf7ba0843576e9
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2025-10-27 13:02:43 +0530

    Time: 30 ms (83.53%), Space: 14.6 MB (91.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,7 +1,7 @@
 import collections
 class Solution(object):
     def subarraySum(self, arr, k):
-        mp=collections.Counter()
+        mp={}
         mp[0]=1
         s=0
         ans=0
@@ -9,5 +9,8 @@ class Solution(object):
             s+=i
             if(s-k in mp):
                 ans+=mp[s-k]
-            mp[s]+=1
+            if(s in mp):
+                mp[s]+=1
+            else:
+                mp[s]=1 
         return ans
