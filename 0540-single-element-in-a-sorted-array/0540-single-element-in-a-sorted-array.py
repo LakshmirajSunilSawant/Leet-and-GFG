cfee2c4f076900883a6f670c48cea5197458b9f9
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2026-01-20 17:08:55 +0530

    Time: 0 ms (100%), Space: 16.4 MB (69.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def singleNonDuplicate(self, nums):
-        
         left, right = 0, len(nums) - 1
 
         while left < right:
@@ -8,11 +7,14 @@ class Solution(object):
 
             if mid % 2 == 1:
                 mid -= 1
-        
+
             if nums[mid] == nums[mid + 1]:
                 left = mid + 2
+
             else:
-                right = mid
+                right = mid 
 
         return nums[left]
         
+    
+         
