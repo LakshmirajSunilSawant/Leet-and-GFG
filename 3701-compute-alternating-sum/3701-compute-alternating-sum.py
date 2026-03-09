e324e7bc9dcf8eae077dee4a358d0fc4bb44e578
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2026-03-09 21:46:48 +0530

    Time: 0 ms (100%), Space: 12.5 MB (51.9%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def alternatingSum(self, nums):
+        res = 0
+
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                res += nums[i]
+            else:
+                res -= nums[i]
+
+        return res
+
+        
