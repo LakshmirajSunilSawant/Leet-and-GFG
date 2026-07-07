408aa5696771519a2ce0bfafef4201571c09b7a3
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2026-07-07 19:19:15 +0530

    Time: 1 ms (99.17%), Space: 42.6 MB (60.45%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,17 +1,16 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        int freq[] = new int[10];
+            int freq[] = new int [10];
+            int ans = 0;
 
-        while (n>0){
-            freq[n % 10]++;
-            n /= 10;
-        }
-
-        int ans = 0;
-        for (int i = 0; i < 10; i++){
-            ans += i * freq[i];
-        }
-
-        return ans;
+            while (n>0){
+                freq[n%10]++;
+                n = n/10;
+            }
+            for(int i =0; i < 10;i++){
+                ans += i * freq[i];
+            }
+            return ans;
     }
+
 }
