07676298e316c38df4aeb6a8088ff2280fb506e2
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2026-07-13 09:08:38 +0530

    Time: 124 ms (42.41%), Space: 46.3 MB (94.94%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -3,19 +3,18 @@ class Solution {
         int n = boxes.length();
         int ans[] = new int[n];
 
-        for( int i = 0; i < n; i++){
-
+        for(int i = 0; i < n; i++){
             int ops = 0;
 
-            for (int j = 0; j < n; j++){
-                if(boxes.charAt(j) == '1'){
+            for(int j = 0; j < n; j++){
+                if (boxes.charAt(j) == '1'){
                     ops += Math.abs(i-j);
                 }
+                
             }
 
             ans[i] = ops;
         }
-
         return ans;
 
     }
