800b6874a9a08d68e2839c493f7439848525ec4f
Lakshmiraj Sunil Sawant <113837667+LakshmirajSunilSawant@users.noreply.github.com>
2026-08-31 19:58:03 +0530

    Time: 604 ms (70.59%), Space: 22.3 MB (54.28%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,25 +8,29 @@ class Solution(object):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            left, right = i+1, n-1
+            left = i + 1
+            right = n - 1
             while left < right:
-                total = nums[i] + nums[right] + nums[left]
+                total  = nums[i] + nums[left] + nums[right]
 
                 if total > 0:
                     right -= 1
                 elif total < 0:
                     left += 1
                 else:
-                    res.append([nums[i],nums[left],nums[right]])
+                    res.append([nums[left],nums[i],nums[right]])
+
 
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right - 1]:
+
+                    while left < right  and nums[right] == nums[right - 1]:
                         right -= 1
 
                     left += 1
                     right -= 1
 
         return res
+            
 
         
